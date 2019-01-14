10595aac7208d445d5497ec375f6b1b47ef1ddee
Isaac Lunsford <isaaclunsford@isaacs-macbook-pro.local>
2019-01-14 14:02:59 -0500

    Initial Commit

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Isaac Lunsford on 1/14/19.
+//  Copyright © 2019 Isaac Lunsford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
